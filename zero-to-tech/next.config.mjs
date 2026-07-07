7a876e04aaceb1ebc16817db5f3cbd1d9ba59ab5
Apricity <apricity.hu@foxmail.com>
2026-07-07 17:35:15 +0800

    next.js支持静态构建

--- zero-to-tech/next.config.mjs
+++ zero-to-tech/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // 以静态站点的形式部署，将 Next.js 的输出模式设置为静态导出
+    // 设置 output: 'export' 后，next build 会把所有页面预渲染为纯 HTML 文件，存放在 out/ 目录中。
+    output: 'export',
+};
 
 export default nextConfig;
